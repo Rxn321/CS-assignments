093af6297a2819e03ab5d894681e07aea1de1837
2093779 <2093779@08-A103-MJ29310.SD41.ADS>
2021-02-25 11:24:18 -0800

    loop

---
+++ SC3/src/loop.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub}
+	//Question 1
+		int i = 100;
+		while(i>=10) {
+			System.out.println("i is " +i);
+			i--;
+		}
+			System.out.println("i is "+i); 
+	//Question 2
+			int a = 1;
+			int j = 1;
+			for (a = 1; a <= 10; i++) 
+			{ 
+			  for (j = 1 ; j <= i; j++) 
+			  { 
+			    System.out.print(j + " "); 
+			  }  
+			System.out.println(""); 
+			} 
+
+			 
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			}
+			 
+	}
