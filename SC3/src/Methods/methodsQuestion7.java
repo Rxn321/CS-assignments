05ef1e3825c63128928b306a427b3a098808612e
Rxn321 <79341425+Rxn321@users.noreply.github.com>
2021-04-07 10:47:45 -0700

    Update methodsQuestion7.java

--- SC3/src/Methods/methodsQuestion7.java
+++ SC3/src/Methods/methodsQuestion7.java
@@ -4,29 +4,40 @@ import java.util.Scanner;
 
 public class methodsQuestion7 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Scanner diceRolls = new Scanner(System.in);
-		System.out.println("How many dice do you want to roll?:");
-		int howMany = diceRolls.nextInt();
-		
-		Scanner diceSides = new Scanner(System.in);
-		System.out.println("How many sides do these dice have?:");
-		int sides = diceSides.nextInt();		
-		
-		Scanner again=new Scanner(System.in);
-		System.out.println("Again?[y,n]");
-		
 
-		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			//Question 1
+			
+			Scanner again = new Scanner(System.in);
+			System.out.println("Again?[y,n]");
+			String yn = again.next();
+			
+			Scanner diceSides = new Scanner(System.in);
+			System.out.println("How many sides do these dice have?:");
+			int sides = diceSides.nextInt();		
+			
+			Scanner diceRolls = new Scanner(System.in);
+			System.out.println("How many dice do you want to roll?:");
+			int howMany = diceRolls.nextInt();
+			
+			int n=1;
+			if n>howManyDice;
+			
+				int max = 20;
+				int min = 1;
+				
+				int rDices =(int)(Math.random()* (max - min + 1) + min);
+				n++;
 
+			
 
-	public static double  rollDies(
 			
 			
-		
-	public static boolean yesOrNo(Scanner ans)
-	}
+			if yn = ;
+			
+		public static int rollDices(int diceOut)
+			System.out.println("diceOut");
+			}
 
-}
+		}
