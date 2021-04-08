7b36a7880b41639ca74aafbc621dd5006d938b03
Rxn321 <79341425+Rxn321@users.noreply.github.com>
2021-04-08 11:03:40 -0700

    Update methodsQuestion7.java

--- SC3/src/Methods/methodsQuestion7.java
+++ SC3/src/Methods/methodsQuestion7.java
@@ -1,43 +1,45 @@
-package Methods;
 
 import java.util.Scanner;
 
-public class methodsQuestion7 {
+public class temp {
 
-
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
-			//Question 1
-			
-			Scanner again = new Scanner(System.in);
-			System.out.println("Again?[y,n]");
-			String yn = again.next();
-			
-			Scanner diceSides = new Scanner(System.in);
-			System.out.println("How many sides do these dice have?:");
-			int sides = diceSides.nextInt();		
-			
-			Scanner diceRolls = new Scanner(System.in);
-			System.out.println("How many dice do you want to roll?:");
-			int howMany = diceRolls.nextInt();
-			
-			int n=1;
-			if n>howManyDice;
-			
-				int max = 20;
-				int min = 1;
-				
-				int rDices =(int)(Math.random()* (max - min + 1) + min);
-				n++;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Question 1	
+		
+		Scanner diceSides = new Scanner(System.in);
+		System.out.println("How many sides do these dice have?:");
+		int sides = diceSides.nextInt();		
+		
+		Scanner diceRolls = new Scanner(System.in);
+		System.out.println("How many dice do you want to roll?:");
+		int howMany = diceRolls.nextInt();
+		diceRoller(sides, howMany);
+	}
 
 			
-
-			
-			
-			if yn = ;
-			
-		public static int rollDices(int diceOut)
-			System.out.println("diceOut");
-			}
-
+	public static void diceRoller(int sides, int howMany)	{	
+		//Dice roller
+	
+		int x;
+		System.out.println("You rolled");
+		for (int i =0; i<howMany; i++) {
+			x = (int)(Math.random()*sides +1);
+			System.out.println(x);
 		}
+		
+		again();
+	}
+		
+	public static void again() {
+		
+		Scanner repeat = new Scanner(System.in);
+		System.out.println("Again?[y,n]");
+		String yn = repeat.next();
+		
+		if (yn.equals("y"))
+			System.out.println("y");
+			
+
+	}
+}	
