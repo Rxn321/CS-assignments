f706f77812a2d0d9c4cf4c4d516dddbabb5c5aa1
Rxn321 <79341425+Rxn321@users.noreply.github.com>
2021-04-07 18:22:20 -0700

    Update methodsQuestion6.java

--- SC3/src/Methods/methodsQuestion6.java
+++ SC3/src/Methods/methodsQuestion6.java
@@ -2,14 +2,19 @@
 
 public class methodsQuestion6 {
 
-	public static void displayBox(int height, int lengh) {
-		height=6;
-		lengh=3;
-	}
 	public static void main(String[] args) {
-		System.out.println("*");
-			
+		int height=3;
+		int width=6;
+		displayBox(height, width);
+	}
+	public static void displayBox(int height, int width) {
+		int n,m;
+		
+		for (m = 1 ; m <= height; m++) { 
+		for ( n=1; n <= width; n++) { 
+		System.out.print("*");}
+		System.out.println("");
 
-			}
+		}
+	}
 }
-
