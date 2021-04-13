8216b01b7fa038c8c2776380aa8bfbc1ae7db554
Rxn321 <79341425+Rxn321@users.noreply.github.com>
2021-04-13 11:16:11 -0700

    Update methodsQuestion7.java

--- SC3/src/Methods/methodsQuestion7.java
+++ SC3/src/Methods/methodsQuestion7.java
@@ -1,12 +1,12 @@
-
+package Methods;
 import java.util.Scanner;
 
-public class temp {
+public class methodsQuestion7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Question 1	
-		
+		do {
 		Scanner diceSides = new Scanner(System.in);
 		System.out.println("How many sides do these dice have?:");
 		int sides = diceSides.nextInt();		
@@ -15,31 +15,38 @@ public class temp {
 		System.out.println("How many dice do you want to roll?:");
 		int howMany = diceRolls.nextInt();
 		diceRoller(sides, howMany);
+	} 
+		while(again());
 	}
-
 			
 	public static void diceRoller(int sides, int howMany)	{	
 		//Dice roller
-	
-		int x;
-		System.out.println("You rolled");
+		int x=0;
+		int sum=0;
+		System.out.print("You rolled:");
 		for (int i =0; i<howMany; i++) {
 			x = (int)(Math.random()*sides +1);
-			System.out.println(x);
+			sum=sum+x;
+			System.out.print(+x+",");
 		}
-		
-		again();
+		System.out.println();
+		System.out.println("Total:"+sum);
 	}
 		
-	public static void again() {
+	public static boolean again() {
 		
 		Scanner repeat = new Scanner(System.in);
+		System.out.println("");
 		System.out.println("Again?[y,n]");
 		String yn = repeat.next();
 		
-		if (yn.equals("y"))
-			System.out.println("y");
-			
-
+		if (yn.equals("y")) {
+			return true;
+		}
+		System.out.println("GoodBye!");
+		return false;
+		
+		
+		
 	}
-}	
+}
